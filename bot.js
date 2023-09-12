2c655796e8a0d7ee8b956474edc1798745144eb1
Cyril Pereira <cyril.pereira@gmail.com>
2023-09-12 22:53:28 +0200

    fix: updated nickname

--- bot.js
+++ bot.js
@@ -122,7 +122,7 @@ const allServices = [ServiceQr, ServiceLol];
 client.on(Events.MessageCreate, (message) => {
   // const channel = message.channel.name;
   const text = message.content;
-  const nickname = message.author.username;
+  const nickname = message.author.nickname;
   // skip bot message
   if (!message.author.bot) {
     // process all services
